acbd4176b23e8137ad9cdd3641911a2ab742c88a
weijie <walkskyer@88jianshen.com>
2013-11-12 17:20:36 +0800

    测试pyQT功能。

--- pyqt4/ui/button.py
+++ pyqt4/ui/button.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'D:\PycharmProjects\pytest27\pyqt4\ui\button.ui'
 #
-# Created: Tue Nov 05 12:41:13 2013
+# Created: Tue Nov 12 17:08:02 2013
 #      by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -32,6 +32,9 @@ class Ui_Dialog(object):
         self.buttonBox.setOrientation(QtCore.Qt.Vertical)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.pushButton = QtGui.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(90, 40, 93, 28))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
 
         self.retranslateUi(Dialog)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), Dialog.accept)
@@ -40,11 +43,12 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.pushButton.setText(_translate("Dialog", "PushButton", None))
 
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)
-    Dialog = QtGui.QMainWindow()
+    Dialog = QtGui.QDialog()
     ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()

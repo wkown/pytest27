75dc6bc3958fd8dec7d73dc911687f84c87a58ba
walkskyer <walkskyer@qq.com>
2014-02-23 11:22:10 +0800

    PyQt4 Tutorial 菜单和工具栏 放在一起

---
+++ pyqt4/tutorial/2/test2-4.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+# test2-4.py
+__author__ = 'adminer'
+"""
+放在一起
+"""
+import sys
+from PyQt4 import QtGui, QtCore
+
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+
+        self.resize(350, 250)
+        self.setWindowTitle('mainWindow')
+
+        textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exit=QtGui.QAction(QtGui.QIcon('exit.png'), 'Exit', self)
+        exit.setShortcut('Ctrl+Q')
+        exit.setStatusTip('Exit application')
+        self.connect(exit, QtCore.SIGNAL('triggered()'),QtCore.SLOT('close()'))
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        file = menubar.addMenu('&File')
+        file.addAction(exit)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exit)
+
+app = QtGui.QApplication(sys.argv)
+main = MainWindow()
+main.show()
+sys.exit(app.exec_())

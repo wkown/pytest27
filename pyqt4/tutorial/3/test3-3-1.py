4b01a7ba9d64da469847c5674d9ff1d5e3d793d8
walkskyer <walkskyer@qq.com>
2014-02-23 18:46:41 +0800

    PyQt4 Tutorial 布局管理 网格布局1

---
+++ pyqt4/tutorial/3/test3-3-1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# test3-3.py
+__author__ = 'adminer'
+"""
+QGridLayout网格布局
+"""
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Grid layout')
+
+        names=['Cls', 'Bck', '', 'Close', '7', '8', '9', '/',
+            '4', '5', '6', '*', '1', '2', '3', '-',
+            '0', '.', '=', '+']
+
+        grid = QtGui.QGridLayout()
+
+        j = 0
+        pos=[(0, 0), (0, 1), (0, 2), (0, 3),
+                (1, 0), (1, 1), (1, 2), (1, 3),
+                (2, 0), (2, 1), (2, 2), (2, 3),
+                (3, 0), (3, 1), (3, 2), (3, 3 ),
+                (4, 0), (4, 1), (4, 2), (4, 3)]
+        for i in names:
+            button = QtGui.QPushButton(i)
+            if j == 2:
+                grid.addWidget(QtGui.QLabel(''), 0, 2)
+            else:
+                grid.addWidget(button, pos[j][0], pos[j][1])
+            j = j + 1
+
+        self.setLayout(grid)
+
+app = QtGui.QApplication(sys.argv)
+ex = Example()
+ex.show()
+sys.exit(app.exec_())

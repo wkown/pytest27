a7ef85745b16979f55cf2d0b5d6b85488fe2c7ec
walkskyer <walkskyer@qq.com>
2014-03-05 22:12:04 +0800

    PyQt4 Tutorial 绘图 绘制文本

--- pyqt4/tutorial/9/test9-1.py
+++ pyqt4/tutorial/9/test9-1.py
@@ -25,14 +25,12 @@ class Example(QtGui.QWidget):
   \u0435\u0432\u0438\u0447 \u0422\u043e\u043b\u0441\u0442\u043e\u0439: \n\
   \u0410\u043d\u043d\u0430 \u041a\u0430\u0440\u0435\u043d\u0438\u043d\u0430'
 
-
     def paintEvent(self, event):
 
         qp = QtGui.QPainter()
         qp.begin(self)
         self.drawText(event, qp)
         qp.end()
-
 
     def drawText(self, event, qp):
 

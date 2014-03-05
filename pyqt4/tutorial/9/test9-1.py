58ff512b6d8dd045741f0eceee677228c884ac5d
walkskyer <walkskyer@qq.com>
2014-03-05 22:00:08 +0800

    PyQt4 Tutorial 绘图 绘制文本

---
+++ pyqt4/tutorial/9/test9-1.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# filename:test9-1.py
+# datetime:2014-03-05 21:59
+__author__ = 'walkskyer'
+"""
+绘制文本
+"""
+import sys
+from PyQt4 import QtGui, QtCore
+
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Draw Text')
+
+        self.text = u'\u041b\u0435\u0432 \u041d\u0438\u043a\u043e\u043b\u0430\
+  \u0435\u0432\u0438\u0447 \u0422\u043e\u043b\u0441\u0442\u043e\u0439: \n\
+  \u0410\u043d\u043d\u0430 \u041a\u0430\u0440\u0435\u043d\u0438\u043d\u0430'
+
+
+    def paintEvent(self, event):
+
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawText(event, qp)
+        qp.end()
+
+
+    def drawText(self, event, qp):
+
+        qp.setPen(QtGui.QColor(168, 34, 3))
+        qp.setFont(QtGui.QFont('Decorative', 10))
+        qp.drawText(event.rect(), QtCore.Qt.AlignCenter, self.text)
+
+app = QtGui.QApplication(sys.argv)
+ex = Example()
+ex.show()
+app.exec_()
+

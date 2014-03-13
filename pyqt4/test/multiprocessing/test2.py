cc3c9a14e0ad883e6fa2803804ce9a36b0375410
walkskyer <walkskyer@qq.com>
2014-03-13 22:41:23 +0800

    多进程实例

---
+++ pyqt4/test/multiprocessing/test2.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# filename:test2.py
+# datetime:2014-03-13 17:54
+__author__ = 'walkskyer'
+#!/usr/bin/env python
+from multiprocessing import Process
+import os
+import time
+
+
+def sleeper(name, seconds):
+   print 'starting child process with id: ', os.getpid()
+   print 'parent process:', os.getppid()
+   print 'sleeping for %s ' % seconds
+   time.sleep(seconds)
+   print "Done sleeping"
+
+
+if __name__ == '__main__':
+   print "in parent process (id %s)" % os.getpid()
+   p = Process(target=sleeper, args=('bob', 5))
+   p.start()
+   print "in parent process after child process start"
+   print "parent process about to join child process"
+   p.join()
+   print "in parent process after child process join"
+   print "parent process exiting with id ", os.getpid()
+   print "The parent's parent process:", os.getppid()

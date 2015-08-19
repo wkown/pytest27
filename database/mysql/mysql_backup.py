42c8e9ac06fc55968718af5033e48f3098b884f7
walkskyer <walkskyer@qq.com>
2015-08-19 16:52:24 +0800

    修改mysql备份文件备份规则，按照周进行文件命名，循环备份，最多可已有7天的备份数据。

--- database/mysql/mysql_backup.py
+++ database/mysql/mysql_backup.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     file_path = os.path.dirname(os.path.realpath(__file__))
     config.read('%s/mysql_backup.ini' % file_path)
     cfg_common = dict(config.items('common'))
-    datetime = time.strftime('%Y%m%d_%H%M%S')
+    datetime = time.strftime('%w_%H%M%S')#按周命名最多可存储最近一周的备份
     for i in xrange(1, 1000):
         try:
             db = dict(config.items('db%s' % i))
-            filename = "%s-%s" % (db['db'], datetime)
+            filename = "%s_%s" % (db['db'], datetime)
             cmd = "%smysqldump --opt -u %s --password=%s %s > %s/%s.sql" % (
                 cfg_common['mysqldump_path'], db['user'], db['pwd'], db['db'], cfg_common['path'], filename)
             #print cmd

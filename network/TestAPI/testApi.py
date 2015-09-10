9371f40a4fea811516192649f75c58503f6bdac3
walkskyer <walkskyker@qq.com>
2015-09-10 22:20:02 +0800

    完善api 测试程序配置信息解析能力

--- network/TestAPI/testApi.py
+++ network/TestAPI/testApi.py
@@ -40,7 +40,10 @@ class HttpClient:
             self.response = None
         return None
 
+
 class ApiConfig:
+    """ api config object
+    """
     def __init__(self,filename):
         if os.path.isfile(filename):
             self.filename = filename
@@ -56,14 +59,65 @@ class ApiConfig:
         f = open(filename, 'rb')
         json_str = f.read()
         f.close()
-        self.config = json.loads(re.sub('\/\*.*?\*\/', '', json_str))
+        self.config = json.loads(re.sub('/\*.*?\*/', '', json_str))
+
+    def get_item(self, url, group):
+        if self.config['api'].has_key(group) and len(self.config['api'][group]['items']) > 0:
+            for item in self.config['api'][group]['items']:
+                if url == item['url']:
+                    return ApiItem(item)
+        return None
+
+    def get_group(self,group):
+        if self.config['api'].has_key(group):
+            return self.config['api'][group]
+        return None
+
+    def get_common_config(self, key):
+        if key in self.config:
+            return self.config[key]
+        return None
+
+    def get_datatype(self):
+        return tuple(self.get_common_config('dataType').split('|'))
+
+
+class ApiItem:
+    """ the group item
+    """
+    def __init__(self, item_conf):
+        # interface name
+        self.name = item_conf['name']
+        # interface url
+        self.url = item_conf['url']
+        # param item
+        self.postField = self.getField = {}
+        if 'postField' in item_conf:
+            self.postField = item_conf['postField']
+        if 'getField' in item_conf:
+            self.getField = item_conf['getField']
+
+        # item dataType
+        self.dataType = item_conf['dataType']
+        # item return data
+        self.returnData = item_conf['returnData']
 
 
 if __name__ == "__main__":
 
-    hc = HttpClient(host="webapp.zwj")
-    print hc.post('/app/user/register')
-    print hc.post('/app/user/login')
+    #hc = HttpClient(host="webapp.zwj")
+    #print hc.post('/app/user/register')
+    #print hc.post('/app/user/login')
 
     ac = ApiConfig('./test_template/tpl_test.json')
     print ac.config
+    print ac.get_common_config('host')
+    print ac.get_datatype()
+    print ac.get_group('app')
+    item = ac.get_item('/app/user/register', 'app')
+    print item
+    print item.url
+    print item.name
+    print item.dataType
+    print item.postField
+    print item.getField

5d52914c53059ee9bc393477b7fa963deaa0898e
walkskyer <walkskyer@qq.com>
2015-11-17 15:03:29 +0800

    添加测试http的socket脚本

---
+++ network/socket/http/test1.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+__author__ = 'walkskyer'
+import socket
+"""
+通过socket测试http协议
+"""
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect(('www.taobao.com',80))
+    s.send('GET / http/1.1\r\nHost: www.baidu.com\r\nConnection: close\r\n\r\n')
+    data_buffer = []
+    while True:
+        d = s.recv(1024)
+        if d:
+            data_buffer.append(d)
+        else:
+            break
+    s.close()
+    data = ''.join(data_buffer)
+    header, body = data.split('\r\n\r\n',1)
+    print header
+    print '-------------------------------------------'
+    print '-------------------------------------------'
+    print body

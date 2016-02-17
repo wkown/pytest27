1131c6f689fcc375a0a060a33484a6514b6fcb76
walkskyer <walkskyer@qq.com>
2016-02-17 09:17:19 +0800

    feat(/network/network_course/chapter2):增加http客户端测试2

---
+++ network/network_course/chapter2/test02.py
@@ -0,0 +1,66 @@
+# -*- coding:utf-8 -*-
+__author__ = 'walkskyer'
+"""
+http client test extend from test01
+cmd: test02.py host textport filename
+"""
+import socket, sys, time
+
+if __name__ == "__main__":
+    host = sys.argv[1]
+    textport = sys.argv[2]
+    filename = sys.argv[3]
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error, e:
+        print "Strange error creating socket: %s" % e
+        sys.exit(1)
+
+    # Try parsing it as a mumeric port number
+
+    try:
+        port = int(textport)
+    except ValueError:
+        # That didn't work, so it's probably a protocol name
+        # Look it up instead
+        try:
+            port = socket.getservbyname(textport, 'tcp')
+        except socket.error, e:
+            print "Couldn't find your port: %s" % e
+            sys.exit(1)
+
+    try:
+        s.connect((host, port))
+    except socket.gaierror, e:
+        print "Address-related error connecting to server: %s" % e
+        sys.exit(1)
+    except socket.error, e:
+        print "Connection error: %s" % e
+        sys.exit(1)
+
+    print "sleeping ..."
+    time.sleep(10)
+    print "Continuing"
+
+    try:
+        s.sendall("GET %s HTTP/1.0\r\n\r\n" % filename)
+    except socket.error, e:
+        print "Error sending data: %s" % e
+        sys.exit(1)
+
+    try:
+        s.shutdown(1)
+    except socket.error, e:
+        print "Error sending data (detected by shutdown): %s" % e
+        sys.exit(1)
+
+    while 1:
+        try:
+            buf = s.recv(2048)
+        except socket.error, e:
+            print "Error receiving data: %s" % e
+            sys.exit(1)
+        if not len(buf):
+            break
+        print buf

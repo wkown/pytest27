63b76d84ca570f7d27d5f794ab1294a3e4faf58e
walkskyer <walkskyer@qq.com>
2016-02-24 09:51:52 +0800

    feat(/network/network_course/chapter9):示例1有问题，略过第九章

---
+++ network/network_course/chapter9/trad_gen_simple.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+__author__ = 'walkskyer'
+"""
+标准库有变动
+Traditional Message Generation, Simple --Chapter 9
+trad_gen_simple.py
+This email.MIMEText import MIMEText
+"""
+from email.MIMEText import MimeWriter
+
+if __name__ == "__main__":
+    pass

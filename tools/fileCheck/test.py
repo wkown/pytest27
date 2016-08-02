c36d2905f735c26b808605bda5de40747d3c631c
walkskyer <walkskyer@qq.com>
2016-08-02 11:12:09 +0800

    feat(tools/fileCheck):调整目录结构

--- tools/fileCheck/test.py
+++ tools/fileCheck/test.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
-import sign
-
+from util import sign
 
 if __name__ == '__main__':
     origin_file = 'test-file/original.html'
@@ -8,6 +7,6 @@ if __name__ == '__main__':
     target_file = 'target-file/original.html'
     sign_val = sign.sign_file(origin_file)
     print sign_val
-    sign.sign2file(sign, origin_file, target_file)
+    sign.sign2file(sign_val, origin_file, target_file)
 
     print sign.sign_check(target_file)

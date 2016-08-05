a75c87020db6de6bb76f86d2c43af3f52f28c91a
walkskyer <walkskyer@qq.com>
2016-08-05 16:07:06 +0800

    feat(tools/fileCheck):添加文件监视程序

---
+++ tools/fileCheck/notify.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#  -*- coding:utf-8 -*-
+import os
+import util.sign as sign
+from  pyinotify import WatchManager, Notifier, \
+    ProcessEvent, IN_DELETE, IN_CREATE, IN_MODIFY
+
+"""
+监听程序，本程序只能在linux环境下测试
+"""
+
+
+class EventHandler(ProcessEvent):
+    """事件处理"""
+
+    def process_IN_CREATE(self, event):
+        file_path = os.path.join(event.path, event.name)
+        file_path = os.path.abspath(file_path)
+        print "Create file: %s " % file_path
+
+    def process_IN_DELETE(self, event):
+        print "Delete file: %s " % os.path.join(event.path, event.name)
+
+    def process_IN_MODIFY(self, event):
+        print "Modify file: %s " % os.path.join(event.path, event.name)
+
+
+def FSMonitor(path='.'):
+    wm = WatchManager()
+    mask = IN_DELETE | IN_CREATE | IN_MODIFY
+    notifier = Notifier(wm, EventHandler())
+    wm.add_watch(path, mask, auto_add=True, rec=True)
+    print 'now starting monitor: %s' % path
+    while True:
+        try:
+            notifier.process_events()
+            if notifier.check_events():
+                notifier.read_events()
+        except KeyboardInterrupt, e:
+            print e
+            notifier.stop()
+            break
+
+
+if __name__ == "__main__":
+    import util.conf as conf
+    cfg = conf.load_config('main', 'config')
+    FSMonitor(cfg.get('file', 'notify_dir'))
--- tools/fileCheck/test_check.py
+++ tools/fileCheck/test_check.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from util import sign
 import os

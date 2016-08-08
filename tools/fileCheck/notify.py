dddc47705a1fbecb363a954f193be363d7c2afd7
walkskyer <walkskyer@qq.com>
2016-08-08 11:14:12 +0800

    feat(tools/fileCheck):增加监测文件写入关闭状态的处理，完美解决文件尺寸过大产生多次修改事件的问题

--- tools/fileCheck/notify.py
+++ tools/fileCheck/notify.py
@@ -4,19 +4,17 @@ import os
 import util.sign as sign
 import util.op as op
 from  pyinotify import WatchManager, Notifier, \
-    ProcessEvent, IN_DELETE, IN_CREATE, IN_MODIFY
+    ProcessEvent, IN_DELETE, IN_CREATE, IN_MODIFY, IN_CLOSE_WRITE
 
 """
 监听程序，本程序只能在linux环境下测试
 """
-
 
 class EventHandler(ProcessEvent):
     """事件处理"""
 
     def process_IN_CREATE(self, event):
         file_path = os.path.abspath(os.path.join(event.path, event.name))
-        #self.check_file(file_path)
         print "Create file: %s " % file_path
 
     def process_IN_DELETE(self, event):
@@ -25,8 +23,12 @@ class EventHandler(ProcessEvent):
 
     def process_IN_MODIFY(self, event):
         file_path = os.path.abspath(os.path.join(event.path, event.name))
-        self.check_file(file_path)
         print "Modify file: %s " % file_path
+
+    def process_IN_CLOSE_WRITE(self,event):
+        file_path = os.path.abspath(os.path.join(event.path, event.name))
+        print "Close file: %s " % file_path
+        self.check_file(file_path)
 
     def check_file(self, file_path):
         """
@@ -40,16 +42,16 @@ class EventHandler(ProcessEvent):
 
         result = sign.sign_check(file_path)
         if not result:
-            print '文件签名错误:%s' % file_path
+            print 'sign file error:%s' % file_path
             op.add_file(file_path)
             return result
-        print '文件签名正确:%s' % file_path
+        print 'sign file ok:%s' % file_path
         return result
 
 
 def FSMonitor(path='.'):
     wm = WatchManager()
-    mask = IN_DELETE | IN_CREATE | IN_MODIFY
+    mask = IN_DELETE | IN_CREATE | IN_MODIFY | IN_CLOSE_WRITE
     notifier = Notifier(wm, EventHandler())
     wm.add_watch(path, mask, auto_add=True, rec=True)
     print 'now starting monitor: %s' % path
@@ -66,5 +68,8 @@ def FSMonitor(path='.'):
 
 if __name__ == "__main__":
     import util.conf as conf
-    cfg = conf.load_config('main', 'config')
+
+    curr_path = os.path.dirname(__file__)
+    cfg = conf.load_config('main', curr_path + '/../config')
+
     FSMonitor(cfg.get('file', 'notify_dir'))

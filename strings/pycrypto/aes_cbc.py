27102111c19e04e4d86ab1d3afdd25c5416182c5
walkskyer <walkskyer@qq.com>
2017-04-13 22:49:28 +0800

    feat(strings/pycrypto):aes

--- strings/pycrypto/aes_cbc.py
+++ strings/pycrypto/aes_cbc.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from Crypto.Cipher import AES
+import base64
 
 __author__ = 'walkskyer'
 
@@ -8,15 +9,20 @@ BS = AES.block_size
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
 unpad = lambda s : s[0:-ord(s[-1])]
 
+key = 'This is a key123'
+iv = 'This is an IV456'
+
+data = 'A really secret message. Not for prying eyes.'
+
 
 # Encryption
-encryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-cipher_text = encryption_suite.encrypt(pad("A really secret message. Not for prying eyes."))
+encryption_suite = AES.new(key, AES.MODE_CBC, iv)
+cipher_text = encryption_suite.encrypt(pad(data))
 print cipher_text
-print cipher_text.encode('hex')
+print base64.b64encode(cipher_text)
 
 # Decryption
-decryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+decryption_suite = AES.new(key, AES.MODE_CBC, iv)
 plain_text = decryption_suite.decrypt(cipher_text)
 print plain_text
 print unpad(plain_text)

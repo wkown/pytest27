a86e979dc1370c0c38ad4923ef65d6500c40bb51
walkskyer <walkskyer@qq.com>
2017-07-03 22:38:25 +0800

    feat(SDK/wxpy):wxpy test01

---
+++ SDK/wxpy/test01.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+__author__ = 'walkskyer'
+"""
+"""
+
+from wxpy import *
+
+
+if __name__ == "__main__":
+    embed()
+    bot = Bot()
+    bot.file_helper.send('hello world')

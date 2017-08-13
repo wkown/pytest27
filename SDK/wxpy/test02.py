7a04b99c6093cd4415aab3c7cde3326606dc75a6
walkskyer <walkskyer@qq.com>
2017-08-13 10:38:11 +0800

    feat(SDK/wxpy): solve msg

---
+++ SDK/wxpy/test02.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+__author__ = 'walkskyer'
+"""
+"""
+from wxpy import *
+import time
+
+
+if __name__ == "__main__":
+    bot = Bot(cache_path='cache/wxpy.pkl')
+
+    @bot.register()
+    def reply(msg):
+        print msg
+        return 'hello'
+
+
+    friend = bot.friends().search(u'书生')[0]
+    while True:
+        friend.send_msg(u'主动发布')
+        time.sleep(20)
+
+embed()

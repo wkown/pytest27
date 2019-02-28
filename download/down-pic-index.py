e06261ab412b2fa089d53c130d1cf574c5e13fb8
walkskyer <walkskyer@qq.com>
2019-02-28 10:45:02 +0800

    使用argparse处理参数

--- download/down-pic-index.py
+++ download/down-pic-index.py
@@ -3,47 +3,51 @@ __author__ = 'walkskyer'
 """
 """
 import requests
-import os, sys, getopt
+import os, sys, getopt, argparse
 
 url_pattern = ''
 start_index = 1
 end_index = 0
+timeout = 60
+output = "./pics"
 
-def usage():
-    print "usage:"
-    sys.exit()
+parse = argparse.ArgumentParser()
+parse.add_argument("-u", "--url-pattern", type=str, help="Url")
+parse.add_argument("-s", "--start-index", default=1, type=int, help="Start Index default 1")
+parse.add_argument("-e", "--end-index", default=0, type=int, help="End Index default 0")
+parse.add_argument("-o", "--output", default="./pics", type=str, help="The output dir")
+parse.add_argument("--timeout", default=60, type=int, help="Timeout")
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'hu:s:e:', ["url-pattern=", "start-index=", "end-index="])
-except getopt.GetoptError:
-    usage()
+args = parse.parse_args()
+if args.url_pattern != "":
+    url_pattern = args.url_pattern
+if args.start_index:
+    start_index = args.start_index
+if args.end_index:
+    end_index = args.end_index
+if args.timeout:
+    timeout = args.timeout
+else:
+    timeout = None
+if args.output != "":
+    output = args.output
 
-if not len(opts):
-    usage()
-
-for opt, argv in opts:
-    if opt in ("-h"):
-        usage()
-    if opt in ("-u", "--url-pattern"):
-        url_pattern = argv
-    if opt in ("-s", "--start-index"):
-        start_index = argv
-    if opt in ("-e", "--end-index"):
-        end_index = int(argv)
-
-target_dir = "./pics"
-if not os.path.isdir(target_dir):
-    os.mkdir(target_dir)
+if not os.path.isdir(output):
+    os.mkdir(output)
 
 for i in xrange(start_index, end_index+1):
     url = url_pattern.replace('*', str(i))
     print url
     filename = os.path.basename(url)
-    path_file = "%s/%s" % (target_dir, filename)
+    path_file = "%s/%s" % (output, filename)
     if os.path.isfile(path_file):
         continue
 
-    req = requests.get(url)
+    req = requests.get(url, timeout=timeout)
+    if req.status_code != 200:
+        print "Request Code: %s" % req.status_code
+        continue
+
     with open(path_file, 'wb') as f:
         f.write(req.content)
         f.close()
